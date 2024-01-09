7c4dfbd5d3531ebd9e1e0d2d32d04ad560f6268d
Tri <ductri.vu@gmail.com>
2024-01-09 16:05:04 +0100

    50 questions in C# (2)

--- Training/Training3-50questions/Program.cs
+++ Training/Training3-50questions/Program.cs
@@ -12,6 +12,40 @@ namespace Training3_50questions
     {
         static void Main(string[] args)
         {
+            Q08();
         }
+
+
+        public class ClassQ08
+        {
+            // Membre de classe statique
+            public static int Compteur { get; set; }
+
+            // Constructeur statique
+            static ClassQ08()
+            {
+                Compteur = 0;
+                Console.WriteLine("Le constructeur statique a été appelé.");
+            }
+
+            public ClassQ08()
+            {
+                Compteur++;
+            }
+        }
+
+        static void Q08()
+        {
+            // Accès à la classe et à son membre statique
+            Console.WriteLine("Compteur avant l'instance : " + ClassQ08.Compteur);
+
+            // Création d'instances de la classe
+            //ClassQ08 instance1 = new ClassQ08();
+            //ClassQ08 instance2 = new ClassQ08();
+
+            // Accès au membre statique après les instances
+            Console.WriteLine("Compteur après les instances : " + ClassQ08.Compteur);
+        }
+
     }
 }
