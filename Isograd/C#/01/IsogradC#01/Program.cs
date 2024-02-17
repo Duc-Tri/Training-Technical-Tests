36568154d525f5ac3d79e7291fd3a4923e45707d
Tri <ductri.vu@gmail.com>
2024-02-17 23:57:38 +0100

    ISOGRAD - c#01 completed (to test)

---
+++ Isograd/C#/01/IsogradC#01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace IsogradC_01
+{
+    internal class IsogradC_01
+    {
+        //---------------------------------------------------------------------
+
+        public struct TypeByValue
+        {
+            public string text;
+            public string Text
+            {
+                get { return text; }
+                set { }
+            }
+        }
+
+        public class TypeByReference
+        {
+            public string Text { get; set; }
+        }
+
+        //---------------------------------------------------------------------
+        public void MyMethod(TypeByValue valObj, TypeByReference refObj)
+        {
+            valObj.Text = "NewValue";
+            Console.WriteLine("valObj.Text = " + valObj.Text);
+            refObj.Text = "NewValue";
+            Console.WriteLine("refObj.Text = " + refObj.Text);
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IsogradC_01.TypeByReference r = new IsogradC_01.TypeByReference();
+
+            (new IsogradC_01()).MyMethod(new IsogradC_01.TypeByValue(), r);
+        }
+    }
+}
+
+
