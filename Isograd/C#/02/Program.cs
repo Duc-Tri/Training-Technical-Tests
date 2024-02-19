5e330d08177aa20708a08acd1e392e4a8dc4f14b
Tri <ductri.vu@gmail.com>
2024-02-20 00:10:59 +0100

    ISOGRAD - c#02 end

--- Isograd/C#/02/Program.cs
+++ Isograd/C#/02/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,21 @@ namespace IsogradC_02
 {
     internal class Program
     {
+        private void ShowFileContent(string filePath)
+        {
+            //-----------------------------------------------------------------
+
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine(File.ReadAllText(filePath));
+            }
+
+            //-----------------------------------------------------------------
+        }
+
         static void Main(string[] args)
         {
+            (new Program()).ShowFileContent("text.txt");
         }
     }
 }
