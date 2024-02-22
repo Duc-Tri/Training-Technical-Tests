ad790f05af8fdf12845caae97074702324846def
Tri <ductri.vu@gmail.com>
2024-02-23 00:40:11 +0100

    ISOGRAD - c#03 end

--- Isograd/C#/03/Program.cs
+++ Isograd/C#/03/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace IsogradC_03
 {
@@ -15,27 +12,56 @@ namespace IsogradC_03
                 actionDelegate();
                 value = 0;
             }
-            catch
-            {
-                value = 0;
-            }
-
             //-----------------------------------------------------------------
-
+            catch (FileNotFoundException e)
+            {
+                value = 1;
+                throw e;
+                throw; // met à jour la propriété StackTrace de e.
+            }
+            catch (Exception)
+            {
+                value = 2;
+                throw; // conserve la trace de pile d’origine de l’exception, qui est stockée dans la propriété Exception.StackTrace
+            }
             //-----------------------------------------------------------------
 
         }
     }
-
-
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            int val = 0;
-            Action action = () => val = 0;
-            (new Question()).MyMethod(action, val);
+            int val = -1;
+
+            Action fileNotFoundAction = () =>
+            {
+                FileStream fs = File.OpenRead("_non_existing_file_");
+            };
+
+            Action otherException = () =>
+            {
+                int b = 0;
+                int a = 1 / b;
+            };
+
+            Action noProblem = () => { };
+
+            try
+            {
+                (new Question()).MyMethod(otherException, out val);
+            }
+            catch
+            {
+            }
+            finally
+            {
+                Console.WriteLine("val = " + val);
+            }
+
         }
+
     }
+
 }
