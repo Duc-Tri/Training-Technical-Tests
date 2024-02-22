d5eae03c83b595396ee6dbf2373938586865b1f7
Tri <ductri.vu@gmail.com>
2024-02-22 23:53:07 +0100

    ISOGRAD - c#03 ongoing

---
+++ Isograd/C#/03/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsogradC_03
+{
+    public class Question
+    {
+        public void MyMethod(Action actionDelegate, out int value)
+        {
+            try
+            {
+                actionDelegate();
+                value = 0;
+            }
+            catch
+            {
+                value = 0;
+            }
+
+            //-----------------------------------------------------------------
+
+            //-----------------------------------------------------------------
+
+        }
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int val = 0;
+            Action action = () => val = 0;
+            (new Question()).MyMethod(action, val);
+        }
+    }
+}
