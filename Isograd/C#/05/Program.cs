05ffe88858844ff05c8c31a307820133925bc3e8
Tri <ductri.vu@gmail.com>
2024-02-24 00:25:13 +0100

    ISOGRAD - c#05 end (read link below)
    https://csharpindepth.com/Articles/Singleton

---
+++ Isograd/C#/05/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsogradC_05
+{
+    public class Question
+    {
+
+        public class SingletonClass
+        {
+            private static SingletonClass _current;
+
+            private SingletonClass()
+            {
+            }
+
+            //-----------------------------------------------------------------
+            public static readonly object padlock = new object();
+            public static SingletonClass Current
+            {
+                get
+                {
+                    if (_current == null)
+                    {
+                        lock (padlock)
+                        {
+                            if (_current == null)
+                                _current = new SingletonClass();
+                        }
+                    }
+                    return _current;
+                }
+            }
+            //-----------------------------------------------------------------
+        }
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
