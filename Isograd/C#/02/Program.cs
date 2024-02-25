d2442ba7b76733af37fa2bf0ee5fd386ed8c3bbf
Tri <ductri.vu@gmail.com>
2024-02-25 18:07:05 +0100

    refacto IsogradC#02 / 04 / 05

--- Isograd/C#/02/Program.cs
+++ Isograd/C#/02/Program.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IsogradC_02
 {
-    internal class Program
+    internal class Question
     {
-        private void ShowFileContent(string filePath)
+        public void ShowFileContent(string filePath)
         {
             //-----------------------------------------------------------------
 
@@ -20,10 +16,13 @@ namespace IsogradC_02
 
             //-----------------------------------------------------------------
         }
+    }
+    internal class Program
+    {
 
         static void Main(string[] args)
         {
-            (new Program()).ShowFileContent("text.txt");
+            (new Question()).ShowFileContent("text.txt");
         }
     }
 }
--- Isograd/C#/05/Program.cs
+++ Isograd/C#/05/Program.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IsogradC_05
+﻿namespace IsogradC_05
 {
     public class Question
     {
-
         public class SingletonClass
         {
             private static SingletonClass _current;
@@ -39,11 +32,11 @@ namespace IsogradC_05
 
     }
 
-
     internal class Program
     {
         static void Main(string[] args)
         {
+            // missing unit tests !
         }
     }
 }
