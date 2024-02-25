f7401d2d125b4bff539e8819ec4ea8fa97ee8878
Tri <ductri.vu@gmail.com>
2024-02-25 17:40:46 +0100

    ISOGRAD - c#06 start

---
+++ Isograd/C#/06/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsogradC_06
+{
+    public class Question
+    {
+        //---------------------------------------------------------------------
+        public void InitializeFileCreationTracker(string pathToTrack)
+        {
+
+        }
+        //---------------------------------------------------------------------
+
+        public string NewFilePath { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Question()).InitializeFileCreationTracker("test");
+        }
+    }
+}
