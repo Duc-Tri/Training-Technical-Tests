8aefff8bc1020a99d002cebe27f6a30a274e7670
Tri <ductri.vu@gmail.com>
2024-03-02 23:39:37 +0100

    ISOGRAD - c#10 begin code

---
+++ Isograd/C#/10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsogradC_10
+{
+    public class Question
+    {
+        public string RemoveDuplicateWords(string value)
+        {
+            //-----------------------------------------------------------------
+
+
+            return null;
+            //-----------------------------------------------------------------
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Question()).RemoveDuplicateWords("One one; two, two Three three Four.");
+        }
+    }
+}
