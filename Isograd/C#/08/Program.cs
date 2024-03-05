d8db61b8864d366becf08e9fca25231f7300d56d
Tri <ductri.vu@gmail.com>
2024-03-05 23:45:37 +0100

    ISOGRAD - c#08 end

--- Isograd/C#/08/Program.cs
+++ Isograd/C#/08/Program.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using static IsogradC_08.Question;
+using System.Xml;
+using System;
 
 namespace IsogradC_08
 {
@@ -12,21 +10,60 @@ namespace IsogradC_08
         public class User
         {
             public string FirstName { get; set; }
-            public string LasstName { get; set; }
+            public string LastName { get; set; }
+            public override string ToString()
+            {
+                return "USER " + FirstName + ", " + LastName;
+            }
         }
 
         /*
-        <?xml version = "1.0" encoding="utf-8".>
+        <?xml version="1.0" encoding="utf-8"?>
         <Users>
-            <User FirstName = "Marc" LastName="Martin"/>
-            <Users FirstName = "John" LastName="Smith"/>
+            <User FirstName="Marc" LastName="Martin" />
+            <User FirstName="John" LastName="Smith" />
         </Users>
         */
 
         public IEnumerable<User> GetUsers(string xmlFilePath)
         {
             //-----------------------------------------------------------------
-            return null;
+            List<User> users = new List<User>();
+            XmlReader reader = new XmlTextReader(xmlFilePath);
+
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element: // The node is an element.
+                        //Console.Write("<" + reader.Name);
+
+                        if (reader.Name.Equals("User"))
+                        {
+                            User u = new User();
+                            while (reader.MoveToNextAttribute()) // Read the attributes.
+                            {
+                                if (reader.Name.Equals("FirstName"))
+                                    u.FirstName = reader.Value;
+                                else if (reader.Name.Equals("LastName"))
+                                    u.LastName = reader.Value;
+                            }
+                            Console.WriteLine(u);
+                        }
+                        break;
+
+                    case XmlNodeType.Text: //Display the text in each element.
+                        Console.WriteLine("*** " + reader.Value);
+                        break;
+
+                    case XmlNodeType.EndElement: //Display the end of the element.
+                        //Console.Write("</" + reader.Name);
+                        break;
+                }
+
+            }
+
+            return users;
             //-----------------------------------------------------------------
         }
     }
@@ -35,6 +72,7 @@ namespace IsogradC_08
     {
         static void Main(string[] args)
         {
+            (new Question()).GetUsers("..\\..\\users.xml");
         }
     }
 }
