9695e03a8d7037e297130938e430705ad00317ef
Duc-Tri <ductri.vu@gmail.com>
2024-04-19 13:57:56 +0200

    IPX2OPC IPX2OPCTable.ConvertRTUName()

--- Training/NPOItest/Program.cs
+++ Training/NPOItest/Program.cs
@@ -1,19 +1,21 @@
-﻿using NPOI.XSSF.UserModel;
+﻿using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 
-Console.WriteLine("OASYS DBEdit XML reader ====================");
+Console.WriteLine("OASYS DBEdit XML reader =================================");
 
 DBEditSchemaWrapper.TestXMLDBEditSchema();
 
-Console.WriteLine("NPOI tests ====================");
+
+Console.WriteLine("NPOI tests ==============================================");
 
 NPOITest.ReadExcel("ExcelOutput.xlsx");
-
 NPOITest.WriteExcel("ExcelOutput.xlsx");
-
 NPOITest.ReadExcel("emprise_poisson.xlsx", "analog");
 
+
+Console.WriteLine("ipx2opc tests ==============================================");
+
 XSSFWorkbook xssfWorkbook = NPOITest.ReadExcel("emprise_poisson.xlsx");
-
 SheetWrapper sheetAnalog = new SheetWrapper(xssfWorkbook, "analog");
-
-
+IPX2OPCanalog ipx2opcAnalog = new IPX2OPCanalog(sheetAnalog.sheet);
+ipx2opcAnalog.ConvertIPX2OPC();
--- Training/NPOItest/ipx2opc/IPX2OPCTable.cs
+++ Training/NPOItest/ipx2opc/IPX2OPCTable.cs
@@ -2,10 +2,10 @@ using NPOI.SS.UserModel;
 
 public abstract class IPX2OPCTable
 {
-    protected ISheet npoiSheet;
+    protected ISheet npoiSheet; // useful ?
+    protected SheetWrapper sheetWrapper;
 
     protected string TableName { get; set; }
-
 
     public abstract void ConvertIPX2OPC();
 
@@ -14,5 +14,17 @@ public abstract class IPX2OPCTable
         npoiSheet = sheet;
     }
 
+    public static string ConvertRTUName(string modbusName)
+    {
+        string opcName = modbusName.Replace("_IPX_", "_OPC_");
+        int indexR = opcName.LastIndexOf("_R");
+        
+        // tryParse ?
+        int num = int.Parse(opcName.Substring(indexR + 2, opcName.Length - indexR - 2));
+        opcName = opcName.Substring(0, indexR) + (num + 10);
+        Console.WriteLine($"ConvertRTUName # old new modbusName=[{modbusName}] opcName=[{opcName}]");
+
+        return opcName;
+    }
 
 }
