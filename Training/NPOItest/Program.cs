120f69fb400b8e0b4bb7af72e9b1ad7b1993cfb0
Tri <ductri.vu@gmail.com>
2024-04-24 22:41:25 +0200

    minor cleaning

--- Training/NPOItest/Program.cs
+++ Training/NPOItest/Program.cs
@@ -1,16 +1,11 @@
 ﻿using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
 
-Console.WriteLine("OASYS DBEdit XML reader =================================");
-
-DBEditSchemaWrapper.TestXMLDBEditSchema();
-
-
 Console.WriteLine("NPOI tests ==============================================");
 
 NPOITest.ReadExcel("ExcelOutput.xlsx");
 NPOITest.TestWriteSomeCellsToExcel("ExcelOutput.xlsx");
-NPOITest.ReadExcel("emprise_poisson.xlsx", "analog");
+NPOITest.ReadExcel("ExcelInput.xlsx", "analog");
 
 Console.WriteLine("ipx2opc tests ==============================================");
 
@@ -18,12 +13,6 @@ XSSFWorkbook xssfWorkbook = NPOITest.ReadExcel("emprise_poisson.xlsx");
 
 if (xssfWorkbook != null)
 {
-    IPX2OPCAnalog ipx2opcAnalog = new IPX2OPCAnalog(new SheetWrapper(xssfWorkbook, "analog"));
-    ipx2opcAnalog.ConvertIPX2OPC();
-
-    IPX2OPCRemote ipx2opcRemote = new IPX2OPCRemote(new SheetWrapper(xssfWorkbook, "remote"));
-    ipx2opcRemote.ConvertIPX2OPC();
-
     NPOITest.WriteExcel(xssfWorkbook, "output_OPC_emprise_poisson.xlsx");
 }
 else
